3d087b4b85cb9d6d1c26edda0a8216ff120fe522
Kavivarshini Kandhasami <kandhasamikavi@gmail.com>
2026-02-27 12:14:21 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,23 @@
+/*
+SCHEMA DESIGN
+SQL Analysis for Global Superstore Retail Sales
+Author: Kavivarshini Kandhasami
+Description: Defines relational constraints and keys
+*/
+-- =========================================
+-- 1. Add Primary Key to People Table
+-- =========================================
 
+ALTER TABLE People
+ADD CONSTRAINT PK_People PRIMARY KEY (Region);
+
+-- =========================================
+-- 2. Add Foreign Key: Orders → People
+-- =========================================
+
+ALTER TABLE Orders
+ADD CONSTRAINT FK_Orders_People
+FOREIGN KEY (Region)
+REFERENCES People(Region);
+
+-- =========================================
